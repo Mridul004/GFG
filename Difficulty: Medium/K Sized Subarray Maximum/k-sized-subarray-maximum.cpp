eaf6853f07558062f71df4769c1b9dd7c2f0f897
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-03-02 09:55:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/K Sized Subarray Maximum/k-sized-subarray-maximum.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        priority_queue<pair<int,int>> pq;
+        vector<int> ans;
+        int n=arr.size();
+        for(int i=0;i<k;i++) pq.push({arr[i],i});
+        ans.push_back(pq.top().first);
+        for(int i=k;i<n;i++){
+            pq.push({arr[i],i});
+            while(!pq.empty()&&abs(pq.top().second-i)>=k) pq.pop();
+            if(!pq.empty()) ans.push_back(pq.top().first);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore newline character after t
+
+    while (t--) {
+        vector<int> arr;
+        int k;
+        string inputLine;
+
+        getline(cin, inputLine); // Read the array input as a line
+        stringstream ss(inputLine);
+        int value;
+        while (ss >> value) {
+            arr.push_back(value);
+        }
+
+        cin >> k;
+        cin.ignore(); // Ignore newline character after k input
+
+        Solution obj;
+        vector<int> res = obj.maxOfSubarrays(arr, k);
+        for (int i = 0; i < res.size(); i++)
+            cout << res[i] << " ";
+        cout << endl;
+        cout << "~"
+             << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
