ee7ec036cb6104c321ae47d7dbd4ec8c9952749e
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-06-10 10:40:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Exactly one swap/exactly-one-swap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int countStrings(string &s) {
+        vector<int> freq(26, 0);
+        for(int i = 0; i < s.length(); i++) freq[s[i] - 'a']++;
+        
+        int sum = freq[25], ans = 0, f = (freq[25] > 1);
+        
+        for(int i = 24; i >= 0; i--) {
+            ans += (freq[i] * sum);
+            sum += freq[i];
+            if(freq[i] > 1) f = 1;
+        }
+        
+        return ans + f;
+    }
+};
