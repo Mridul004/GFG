863590f8ac8cd2dc1ebd1218c59855b6a3402bf0
Mridul Gupta <103638746+Mridul004@users.noreply.github.com>
2025-09-16 08:01:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix Evaluation/postfix-evaluation.cpp
@@ -0,0 +1,42 @@
+class Solution {
+  public:
+    int evaluatePostfix(vector<string>& arr) {
+        stack<int> s;
+        
+        for(auto i: arr){
+            if(i=="*" || i=="+"||i=="-"||i=="/"||i=="^"){
+                int last = s.top();
+                s.pop();
+                int secondLast = s.top();
+                s.pop();
+                
+                
+                
+                
+                if(i=="*")
+                    {
+                        s.push(last*secondLast);
+                    }else if(i=="+"){
+                        s.push(last+secondLast);
+                    }else if(i=="^"){
+                        int power = (int)pow(secondLast,last);
+                        s.push(power);
+                    }else if(i=="-"){
+                        s.push(secondLast-last);
+                    }else{
+                       int division = secondLast / last;
+            int remainder = abs(secondLast) % abs(last);
+            
+            if (remainder != 0 && (last*secondLast)<0) {
+                division--;
+            }
+            s.push(division);
+                    }
+            }else{
+                s.push(stoi(i));
+            }
+        }
+        
+        return s.top();
+    }
+};
